eb4ce91e5d1fc3acaa6b716443765091eae2e430
Sonali Kar <sonali.kar87@gmail.com>
2022-02-10 19:37:51 +0000

    Securing Your App

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -27,7 +27,12 @@ class Bookmark
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+    result = connection.exec_params(
+      "INSERT INTO bookmarks (url, title) VALUES($1, $2) RETURNING id, title, url;", [url, title]
+    )
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 end
+
+# result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+# Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
