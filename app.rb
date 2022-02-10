0f7f0c912019207ef02c3639e2cca6fad523d58e
Sonali Kar <sonali.kar87@gmail.com>
2022-02-10 11:12:53 +0000

    Create a new bookmark

--- app.rb
+++ app.rb
@@ -23,5 +23,17 @@ get '/bookmarks' do
   erb :'bookmarks/index'
 end
 
+get '/bookmarks/new' do
+  erb :"bookmarks/new"
+end
+
+post '/bookmarks' do
+  # url = params['url']
+  # connection = PG.connect(dbname: 'bookmark_manager_test')
+  # connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+  Bookmark.create(url: params[:url])
+  redirect '/bookmarks'
+end
+
   run! if app_file == $0
 end
